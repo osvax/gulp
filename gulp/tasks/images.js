a52e79c915cdba3a04dc52b97bfbaaa2db1b8c12
osvax <osvax@yandex.ru>
2025-02-24 16:33:44 +0400

    edit gulp-imagemin

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -1,15 +1,11 @@
 import gulpif from 'gulp-if';
-import imagemin from 'imagemin';
+import imagemin from 'gulp-imagemin';
 import newer from "gulp-newer";
 
 export const images = () => {
   return app.gulp.src([`${app.paths.srcImgFolder}/**/**.{jpg,jpeg,png,svg}`], { encoding: false })
-    // .pipe(newer(app.paths.buildImgFolder))
-    // .pipe(gulpif(app.isProd, imagemin([
-    //   gifsicle({ interlaced: true }),
-    //   mozjpeg({ quality: 75, progressive: true }),
-    //   optipng({ optimizationLevel: 2 }),
-    // ])))
+     .pipe(newer(app.paths.buildImgFolder))
+     .pipe(gulpif(app.isProd, imagemin()))
     .pipe(app.gulp.dest(app.paths.buildImgFolder))
 };
 
